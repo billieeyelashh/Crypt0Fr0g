e562ad166b263833bb2c02205703998510e50ee3
Adrian <71754820+lona9a@users.noreply.github.com>
2021-07-01 15:02:35 +0200

    Update database.py

--- database.py
+++ database.py
@@ -5,14 +5,14 @@ import typing
 class WorkspaceData:
     def __init__(self):
         self.conn = sqlite3.connect("database.db")
-        self.conn.row_factory = sqlite3.Row  # Makes the data retrieved from the database accessible by their column name
+        self.conn.row_factory = sqlite3.Row  
         self.cursor = self.conn.cursor()
 
         self.cursor.execute("CREATE TABLE IF NOT EXISTS watchlist (symbol TEXT, exchange TEXT)")
         self.cursor.execute("CREATE TABLE IF NOT EXISTS strategies (strategy_type TEXT, contract TEXT,"
                             "timeframe TEXT, balance_pct REAL, take_profit REAL, stop_loss REAL, extra_params TEXT)")
 
-        self.conn.commit()  # Saves the changes
+        self.conn.commit()  
 
     def save(self, table: str, data: typing.List[typing.Tuple]):
 
@@ -22,9 +22,8 @@ class WorkspaceData:
 
         table_data = self.cursor.execute(f"SELECT * FROM {table}")
 
-        columns = [description[0] for description in table_data.description]  # Lists the columns of the table
+        columns = [description[0] for description in table_data.description]  
 
-        # Creates the SQL insert statement dynamically
         sql_statement = f"INSERT INTO {table} ({', '.join(columns)}) VALUES ({', '.join(['?'] * len(columns))})"
 
         self.cursor.executemany(sql_statement, data)
